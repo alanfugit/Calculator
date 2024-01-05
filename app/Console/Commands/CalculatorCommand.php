5a15da2df247bbf9f8cd6b7c9762974a6112a85a
alan.fu <alan@alandeMacBook-Pro.local>
2024-01-05 23:55:55 +0800

    修改不用eval执行运算

--- app/Console/Commands/CalculatorCommand.php
+++ app/Console/Commands/CalculatorCommand.php
@@ -30,12 +30,12 @@ class CalculatorCommand extends Command
             $input = $this->ask('Please enter an expression');
             // 移除所有空格
             $expression = str_replace(' ', '', $input);
-            // try {
+            try {
                 $result = $this->evaluate($expression);
                 $this->line($result);
-            // } catch (\Throwable $e) {
-            //     $this->error('Invalid expression');
-            // }
+            } catch (\Throwable $e) {
+                $this->error('Invalid expression');
+            }
         }
     }
 
@@ -46,41 +46,75 @@ class CalculatorCommand extends Command
      */
     public function evaluate($input): Int
     {
-        //查找最内层的()里面的数据，并运算后替换掉输入字符串
-        $blockFirst = strrpos($input, '('); //从右开始，取得最后的（        
-        $blockEnd = strpos($input, ')', $blockFirst); //根据开始位置匹配最后的关闭标签
-        $operAllStr = substr($input, $blockFirst, $blockEnd - $blockFirst + 1);//截取最里面括号字符串
-        
-        $operStr = str_replace(['(', ')'], '', $operAllStr);
-        $result = $this->operationStr($operStr); //当前计算结果
-        //把结果替换掉优先级括号
-        $repStr = str_replace($operAllStr, $result, $input);
-        //var_dump($result); 
-        if(strpos($repStr, '(') !== false) { //如果还存在括号优先级，递归处理
+        if(strpos($input, '(') !== false) { //检查输入是否包含()括号，包含就取出计算，不包含直接运算
+            //查找最内层的()里面的数据，并运算后替换掉输入字符串
+            $blockFirst = strrpos($input, '('); //从右开始，取得最后的（ 的键      
+            $blockEnd = strpos($input, ')', $blockFirst); //根据开始位置匹配最后的关闭标签
+            $operAllStr = substr($input, $blockFirst, $blockEnd - $blockFirst + 1);//截取最里面括号字符串
+            
+            $operStr = str_replace(['(', ')'], '', $operAllStr); //过滤括号取得运算字符串
+            $result = $this->operationStr($operStr); //当前计算结果
+
+            $repStr = str_replace($operAllStr, $result, $input); //把结果替换掉优先级括号
+
             return $this->evaluate($repStr);
+            
         }else {
-            return $result;
+
+            return $this->operationStr($input);
         }
-        //return $operStr;
         //return eval("return {$input};");
     }
 
     /**
      * 执行运算表达式字符串
      * @param String $operStr
+     * @return Int
      */
     public function operationStr($operStr): Int 
     {
-        preg_match_all('/(\\d+)|([-+()*\/])/', $operStr, $matches);// 用正则解析表达式把数据解出来
         $result = 0; //结果初始化
+
+        preg_match_all('/(\\d+)|([-+()*\/])/', $operStr, $matches);// 用正则解析表达式把数据解出来
         
         $operArr = $matches[0];
 
-        $this->operation($operArr, true); //执行乘法除法
-    
-        $addAndSubOperArr = array_values($operArr); //把操作数组重新生成Key值顺序，剩下的就只是+-法了
+        $result = $this->operation($operArr, true); //执行乘法除法
+        
+        if(count($operArr) > 1) { //如果执行乘和除法还没算完，才继续执行加法运算, 如果执行加法，以+法算完的结果为准
+            $addAndSubOperArr = array_values($operArr); //把操作数组重新生成Key值顺序，剩下的就只是+-法了
+            $result = $this->operation($addAndSubOperArr);
+        }
+        return $result;
+    }
 
-        $result = $this->operation($addAndSubOperArr);
+    /**
+     * 运算结果
+     * @param Array &$array 引用数组，正则匹配后的匹配数组
+     * @param Boolean $priority 控制是否先执行乘法和除法高优先级运算
+     * @return Int
+     */
+    public function operation(&$array, $priority=false): Int
+    {
+        $arrCount = count($array);
+        $result = 0; //结果初始化
+        for($i = 1; $i < $arrCount; $i+=2) { //表达式运算符都是左右两边都有数字的，取得运算符运算两边的数值计算结果就可以
+             
+             if($priority && in_array($array[$i], ['+', '-'])) { //如果要计算优先级的乘法和除法，跳过本次循环
+                continue;
+             }
+             //取得运算符两边的数字
+             $adjacentArr = $this->getAdjacentKeys($array, $i); 
+             $leftNum = $adjacentArr[0]; //取得运算符左边数值
+             $rightNum = $adjacentArr[1]; //取得运算符右边数值
+             $result = $this->execOper($leftNum, $rightNum, $array[$i]); //计算结果
+
+             //把运算后的数据清除
+             unset($array[$i-1], $array[$i], $array[$i+1]);
+             //把结果放进去清除的位置，并不改变原来的键值对,因为添加进去虽然指定了键值，但是排序还是在最后，所以要排序一下
+             $array[$i] = $result;
+             ksort($array);
+        }
 
         return $result;
     }
@@ -115,45 +149,12 @@ class CalculatorCommand extends Command
     }
 
     /**
-     * 运算结果
-     * @param Array &$array 引用数组，正则匹配后的匹配数组
-     * @param Boolean $priority 控制是否先执行乘法和除法
-     * @return Int
-     */
-    public function operation(&$array, $priority=false): Int
-    {
-        $arrCount = count($array);
-        $result = 0; //结果初始化
-        for($i = 1; $i < $arrCount; $i+=2) { //表达式运算符都是左右两边都有数字的，取得运算符运算两边的数值计算结果就可以
-             
-             if($priority && in_array($array[$i], ['+', '-'])) { //如果要计算优先级的乘法和除法，跳过本次循环
-                continue;
-             }
-             //取得运算符两边的数字
-             $adjacentArr = $this->getAdjacentKeys($array, $i); 
-             $leftNum = $adjacentArr[0]; //取得运算符左边数值
-             $rightNum = $adjacentArr[1]; //取得运算符右边数值
-             $result = $this->execOper($leftNum, $rightNum, $array[$i]); //计算结果
-
-             //把运算后的数据清除
-             //var_dump($array);
-             unset($array[$i-1], $array[$i], $array[$i+1]);
-             //把结果放进去清除的位置，并不改变原来的键值对,因为添加进去虽然指定了键值，但是排序还是在最后，所以要排序一下
-             $array[$i] = $result;
-             ksort($array);
-             var_dump($array);
-        }
-        //var_dump($array);
-        return $result;
-    }
-
-    /**
      * 取得数组给定键的两边的值
      * @param Array $array 要操作的数组
      * @param Int $key 数组的某个键值
-     * @return Array
+     * @return Array 反正前面和后面的数值
      */
-    function getAdjacentKeys($array, $key): Array 
+    public function getAdjacentKeys($array, $key): Array 
     {
         $keys = array_keys($array);
         $index = array_search($key, $keys);
